ed53243f230dff698cf5e9af155c8d554869b05f
gmintoco <50625416+gmintoco@users.noreply.github.com>
2022-05-16 13:36:28 +0200

    Develop (#21)
    * Put new LBIP in now its setup properly
    
    * remove space
    
    * Update chart and version (#2)
    
    Co-authored-by: Jen <jhasenau@greenpeace.org>
    
    * Add dockerauth (#4)
    
    * Update chart and version
    
    * add docker auth creds
    
    * try auth again
    
    * try diff auth way
    
    * test change
    
    Co-authored-by: Jen <jhasenau@greenpeace.org>
    
    * add context to lint
    
    * helm3 upgrade
    
    * Upgrade nginx and tune memory requests (#8)
    
    
    * update to latest version
    
    * set min available
    
    *  Setup metric export
    
    * add certificate and set default ssl (#10)
    
    * add certficiate and set default
    
    * appease the linter
    
    * change durations
    
    * remove switz
    
    * Update k8s.greenpeace.org
    
    Update k8s.greenpeace.org
    
    * update memory requests (#12)
    
    * Set proxy-body-size to 20m
    
    This configures nginx max body size to allow uploads for up to 20m
    
    * Update body size again to 60M
    
    Max upload in WP is 50m this gives plenty of overhead.
    
    * Update NGINX Ingress to 3.40.0
    
    * Update chart now we are on 1.19
    
    * Configure default backend service
    
    This configures the default backend service. This means requests to unknown ingress hosts will be served the Greenpeace 404 instead of the default Nginx 404.
    
    * feat: add CF source ips to allow list in YAML file
    
    * fix: update script to use old version of yq
    
    * fix: only use ip4 addresses
    
    * fix: update comments
    
    * fix: add newline
    
    * fix: add newline
    
    * fix: update range script not working
    
    Co-authored-by: Jen <jhasenau@greenpeace.org>
    Co-authored-by: jencub <32348677+jencub@users.noreply.github.com>

--- update_source_range.sh
+++ update_source_range.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 IP_ARRAY=$(./cf_generate_array.sh | jq 'del(.[-1])')
-yq < values.yaml | jq ".controller.service.loadBalancerSourceRanges = $IP_ARRAY" | yq -y
+yq -iy ".controller.service.loadBalancerSourceRanges = $IP_ARRAY" values.yaml
